ccdd42aa507c043a4c2d29971b524b5daeb45f7b
Brandy Moore <brandymoore923@gmail.com>
2022-09-08 10:08:19 -0500

    added questions to index.js and ran in terminal

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'what is the name of your project?',
+        name: 'project'
+    },
+    {
+        type: 'input',
+        message: 'provide a short discription explaining your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'what are the steps required to install the project?',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'what instructions do you have for the user to accurately use your project?',
+        name: 'usage'
+    }
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
